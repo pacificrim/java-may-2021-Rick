811179fc8a859e544428ddeacf241578ac9dd9c0
Rick <ycheroki@gmail.com>
2021-05-21 10:11:00 -0600

    Hello World in Netbean

---
+++ HiWorld/src/main/java/com/rick/hiworld/Hi.java
@@ -0,0 +1,15 @@
+/**
+ *@author rick
+ *email: ycheroki@gmail.com
+ *date: may20/21
+ *purpose: My first Hi World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.rick.hiworld; 
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
